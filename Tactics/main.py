98dba3cfac2f5bfd4561ca2e3e0b38ea34ce4fb6
orez- <bshaginaw120@gmail.com>
2012-04-29 00:13:25 -0400

    Just uploading the three steps i saved em in

--- Tactics/main.py
+++ Tactics/main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import math
-import tile
+from tile import *
 
 # Initialize the game engine
 pygame.init()
@@ -17,12 +17,12 @@ brown = (128, 64,  0)
 pi=3.141592653
 
 # Set the height and width of the screen
-size=(400,500)
+size=(1000,500)
 screen=pygame.display.set_mode(size)
 
-cx = 0
+cx = 0  # player loc
 cy = 0
-dx = 0
+dx = 0  # display loc
 dy = 0
 pygame.display.set_caption("Orez Tactics")
 
@@ -30,52 +30,14 @@ pygame.display.set_caption("Orez Tactics")
 done=False
 clock = pygame.time.Clock()
 
-sqH = 20
-sqW = sqH*2
-sqD = 10
-
-xChunk = 10
-yChunk = 10
-zChunk = 10
-
-boW = (xChunk+yChunk)*sqW/2
-boH = (xChunk+yChunk)*sqH/2
-boD = sqD*zChunk
-
-def setPos():
-    global dx, dy
-    dx = (cx-cy)*sqW/2+(size[0]-boW)/2
-    dy = (cx+cy)*sqH/2+board[cx][cy]*sqD+(size[1]/2-(boH+boD))+18
-
-def drawTile(surface, rx, ry, rz, mx, texture=green):
-    step = 255/max(xChunk,yChunk)
-    base  = (rx*step/2,ry*step/4,50)
-    top = (rx*step,50,ry*step)
-    #base = brown
-    #top  = green
-    
-    surf = pygame.Surface((sqW,rz*sqD+sqH))
-    surf.fill((255,0,255))
-    surf.set_colorkey((255,0,255))  # magenta is see-through
-    
-    pygame.draw.polygon(surf, base, [(0,sqH/2),(sqW/2,0),(sqW,sqH/2),(sqW,sqD*rz+sqH/2),(0,sqD*rz+sqH/2)])
-    pygame.draw.polygon(surf, top,  [(0,sqD*rz+sqH/2),(sqW/2,sqD*rz),(sqW,sqD*rz+sqH/2),(sqW/2,sqD*rz+sqH)])
-    surface.blit(surf, ((mx-1)*sqW/2-((rx-ry)*sqW)/2, ((rx+ry-2)*sqH)/2))
-
+board = Board((20,20,30), (40,20,10), size)
 screen.fill(black)
-moveableBoard = pygame.Surface((boW,boH+boD))
-#moveableBoard.fill(white)
-board = [[min(zChunk,math.floor(((y+1)**2+(x+1)**2)**.5)) for y in xrange(yChunk)] for x in xrange(xChunk)]
-#board = [[min(zChunk,x+y+1) for y in xrange(yChunk)] for x in xrange(xChunk)]
-#board = [[0 for y in xrange(yChunk)] for x in xrange(xChunk)]
+board.setDisplayPosition(cx,cy)
 
-for x in range(xChunk-1,-1,-1):
-    for y in range(yChunk-1,-1,-1):
-        drawTile(moveableBoard, x+1,y+1,board[x][y], xChunk)
-moveableBoard = pygame.transform.flip(moveableBoard, False, True)
-setPos()
+moving = 0
+step = .125 # be careful with this number and floating point rounding....
 
-while done==False:
+while not done:
     # This limits the while loop to a max of 60 times per second.
     # Leave this out and we will use all CPU we can.
     clock.tick(60)
@@ -85,22 +47,44 @@ while done==False:
             done=True # Flag that we are done so we exit this loop
         elif event.type == pygame.KEYDOWN:
             #print event.key
-            if event.key == pygame.K_w:
-                cy = min(yChunk-1,cy+1)
-                setPos()
-            elif event.key == pygame.K_a:
-                cx = min(xChunk-1,cx+1)
-                setPos()
-            elif event.key == pygame.K_s:
-                cy = max(0,cy-1)
-                setPos()
-            elif event.key == pygame.K_d:
-                cx = max(0,cx-1)
-                setPos()
+            if not moving:
+                if event.key == pygame.K_w:
+                    if cy != board.chunk[1]-1:
+                        moving = 1
+                        board.setDisplayPosition(cx,cy)
+                elif event.key == pygame.K_a:
+                    if cx != board.chunk[0]-1:
+                        moving = 2
+                        board.setDisplayPosition(cx,cy)
+                elif event.key == pygame.K_s:
+                    if cy != 0:
+                        moving = 3
+                        board.setDisplayPosition(cx,cy)
+                elif event.key == pygame.K_d:
+                    if cx != 0:
+                        moving = 4
+                        board.setDisplayPosition(cx,cy)
+    
+    if moving:
+        if moving==1:
+            cy += step
+        if moving==2:
+            cx += step
+        if moving==3:
+            cy -= step
+        if moving==4:
+            cx -= step
+        board.setDisplayPosition(cx,cy)
+        if cx==math.floor(cx) and cy==math.floor(cy):
+            moving = 0
     
     screen.fill(black)
-    screen.blit(moveableBoard,(dx,dy))
-    pygame.draw.circle(screen,white,(size[0]/2,size[1]/2),10)
+    board.printToScreen(screen)
+    pygame.draw.polygon(screen,(255,255,255),[(int((size[0]/2)-20), int((size[1]/2)+5)),\
+                                              (int(size[0]/2),      int((size[1]/2)-5)),\
+                                              (int((size[0]/2)+20), int((size[1]/2)+5)),\
+                                              (int(size[0]/2),      int((size[1]/2)+15))], 3)
+    #pygame.draw.circle(screen,white,(size[0]/2,size[1]/2),10)   # draw player
     pygame.display.flip()
 
 # Be IDLE friendly
--- Tactics/tile.py
+++ Tactics/tile.py
@@ -1,4 +1,105 @@
+import pygame
+import math
+import random
+
+class Unit:
+    def __init__(self):
+        pass
+    def display(self,screen,size):
+        ninja = pygame.image.load("idle.png")
+        ninja = pygame.transform.flip(ninja, False, True)
+        screen.blit(ninja,size)
+
 class Tile:
-    def __init__(self,cx,cy):
+    top_img = pygame.transform.flip(pygame.image.load("top.png"), False, True)
+    sqS = ()
+    def __init__(self,cx,cy,cz):
         self.x = cx
         self.y = cy
+        self.z = cz
+        self.unit = (Unit() if random.randint(0,20)<4 else None)
+    def drawTile(self, surface, mx, rx=None, ry=None, rz=None):
+        if Tile.sqS == ():
+            raise Exception("You must define sqS in Tile before using any Tiles")
+        rx = self.x if rx==None else rx
+        ry = self.y if ry==None else ry
+        rz = self.z if rz==None else rz
+        
+        sqW = Tile.sqS[0]
+        sqH = Tile.sqS[1]
+        sqD = Tile.sqS[2]
+        
+        # vvv COLORS!!! vvv
+        step = 255/max(20,20)
+        base  = (rx*step/2,ry*step/4,50)
+        top = (rx*step,50,ry*step)
+        #base = (128,64,0)
+        #top  = (0,128,0)
+        
+        size = (sqW,rz*sqD+sqH)
+        surf = pygame.Surface(size)
+        surf.fill((255,0,255))
+        surf.set_colorkey((255,0,255))  # magenta is see-through
+        
+        pygame.draw.polygon(surf, base, [(0,sqH/2),(sqW/2,0),(sqW,sqH/2),(sqW,sqD*rz+sqH/2),(0,sqD*rz+sqH/2)])
+        surf.blit(Tile.top_img, (0,sqD*rz+1))
+        #pygame.draw.polygon(surf, top,  [(0,sqD*rz+sqH/2),(sqW/2,sqD*rz),(sqW,sqD*rz+sqH/2),(sqW/2,sqD*rz+sqH)])
+        surfloc = ((mx-1)*sqW/2-((rx-ry)*sqW)/2, ((rx+ry-2)*sqH)/2)
+        surface.blit(surf, surfloc)
+        if self.unit != None:
+            self.unit.display(surface,(int(surfloc[0]+(size[0]-25)/2),int(surfloc[1]+size[1]-12)))
+        
+class Board:
+    def __init__(self,chunk_size,square_size,screen_size):
+        self.assertType(chunk_size,  tuple, 3, name="chunk_size")
+        self.assertType(square_size, tuple, 3, name="square_size")
+        self.assertType(screen_size, tuple, 2, name="screen_size")
+        self.sqS = square_size
+        self.chunk = chunk_size
+        self.ssize = screen_size
+        Tile.sqS = self.sqS
+        self.board_size = ((self.chunk[0]+self.chunk[1])*self.sqS[0]/2,\
+                           (self.chunk[0]+self.chunk[1])*self.sqS[1]/2,\
+                           (self.chunk[2]*self.sqS[2]))
+        self.board_data = [[Tile(x+1,y+1,min(self.chunk[2],math.floor(((y+1)**2+(x+1)**2)**.5)))\
+                                for y in xrange(self.chunk[1])]\
+                                    for x in xrange(self.chunk[0])]
+        self.board_image = pygame.Surface((self.board_size[0], self.board_size[1]+self.board_size[2]))
+        self.dx = 0
+        self.dy = 0
+        self.buildDisplay()
+
+    # set the display position based on player coordinates. accepts fractional values
+    # for one coordinate (never both), for a frame in a transition
+    def setDisplayPosition(self,cx,cy):
+        if cx==math.floor(cx) and cy==math.floor(cy):   # you are ON a square
+            zOffset = self.board_data[int(cx)][int(cy)].z
+        else:                                           # you are between squares
+            fx = cx-math.floor(cx)
+            fy = cy-math.floor(cy)
+            if fx and fy:
+                raise TypeError("You may only move the display in one cardinal direction at a time.")
+            zOffset = self.board_data[int(math.floor(cx))][int(math.floor(cy))].z*(1-max(fx,fy))    # linearly average of the two
+            zOffset+= self.board_data[ int(math.ceil(cx))][ int(math.ceil(cy))].z*(  max(fx,fy))
+        
+        self.dx = (cx-cy)*self.sqS[0]/2+(self.ssize[0]-self.board_size[0])/2
+        self.dy = (cx+cy)*self.sqS[1]/2+zOffset*self.sqS[2]+(self.ssize[1]/2-(self.board_size[1]+self.board_size[2]))+18
+
+    def buildDisplay(self):
+        for x in range(self.chunk[0]-1,-1,-1):
+            for y in range(self.chunk[1]-1,-1,-1):
+                self.board_data[x][y].drawTile(self.board_image, self.chunk[0])
+        self.board_image = pygame.transform.flip(self.board_image, False, True)
+        
+    def printToScreen(self,screen):
+        screen.blit(self.board_image,(self.dx,self.dy))
+        
+    def assertType(self, checkitem, checkType, size=-1, **args):
+        name = ""
+        if "name" in args:
+            name = " for "+args["name"]
+        if not (isinstance(checkitem,checkType) and (size==-1 or len(checkitem)==size)):
+            size = str(size)
+            if isinstance(checkitem,checkType):
+                raise TypeError("You must pass a tuple of size "+size+name+" (passed tuple of size "+str(len(checkitem))+")")
+            raise TypeError("You must pass a tuple"+("" if size=="-1" else " of size "+size)+name+" (passed "+str(type(checkitem))+")")
