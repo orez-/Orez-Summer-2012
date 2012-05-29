f84cb58785fbcfbac34124910ddec99c48517e74
orez- <bshaginaw120@gmail.com>
2012-05-29 03:12:32 -0400

    I forget what I did with this.

--- HexiomSolver/hexiom_solver.py
+++ HexiomSolver/hexiom_solver.py
@@ -17,7 +17,7 @@ class HexiomSpot:
         elif value in ".#":
             self.last_value = -1
             self.value = value
-            self.locked = False
+            self.locked = (value == "#")
         else:
             bad_value()
 
@@ -30,7 +30,12 @@ class HexiomSpot:
         return count
 
     def __str__(self):
-        return str(self.value)
+        #return str(self.value)
+        try:
+            int(self.value)
+            return str(self.value - self.last_value)
+        except ValueError:
+            return self.value
 
 class HexiomBoard:
     def __init__(self, side):
@@ -41,19 +46,37 @@ class HexiomBoard:
             for r in xrange(len(board)):
                 if board[r][c] != "#":
                     if i >= len(pattern):
-                        print "Uh... ran out of tokens."
-                        return
+                        board[r][c] = "."
+                        continue
                     board[r][c] = str(pattern[i])
                     i += 1
-        self.board = self.convert_to_spots(board)
+        self.convert_to_spots(board)
+
+    def swap(self, (r1, c1), (r2, c2)):
+        e1 = self.board[r1][c1]
+        e2 = self.board[r2][c2]
+        if e1.locked or e2.locked:
+            return False
+        e1.value == e2.value
 
     def convert_to_spots(self, board):
-        toR = [[HexiomSpot(elem) for elem in row] for row in board]
-        for r, row in enumerate(toR):
+        self.board = [[HexiomSpot(elem) for elem in row] for row in board]
+        for r, row in enumerate(self.board):
             for c, elem in enumerate(row):
-                elem.adjacent = 
-                elem.recount_neighbors()
-        return toR
+                self.build_spot_adj(r, c)
+
+    def build_spot_adj(self, r, c):
+        elem = self.board[r][c]
+        elem.adjacent = []
+        for ra, ca in ((r+1, c), (r-1, c),
+                       (r, c-1), (r, c+1),
+                       (r + 2*(c%2) - 1, c-1),
+                       (r + 2*(c%2) - 1, c+1)):
+            if 0 <= ra < len(self.board) and 0 <= ca < len(self.board[0]):
+                adj = self.board[ra][ca]
+                if str(adj.value) not in ".#":  # it's a number
+                    elem.adjacent.append(adj)
+        elem.recount_neighbors()
 
     def generate_board_shape(self, side):
         size = (side * 2) - 1
@@ -72,16 +95,17 @@ class HexiomBoard:
         return toR
 
     def print_board(self):
+        print ""
         board = self.board
         for r, row in enumerate(board):
             toR1 = ""
-            toR2 = " "
+            toR2 = "  "
             for e,elem in enumerate(row):
                 elem = str(elem)
                 if e%2:
-                    toR2 += elem+" "
+                    toR2 += elem+"   "
                 else:
-                    toR1 += elem+" "
+                    toR1 += elem+"   "
             print toR1
             print toR2
 
