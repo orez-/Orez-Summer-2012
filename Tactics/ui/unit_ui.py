5c36fbf0bf1f7a54e8b8f9d63111dd76463434e0
orez- <bshaginaw120@gmail.com>
2012-05-30 01:22:20 -0400

    Some equip-screen; some equipping logic.
    Currently we have no sense of "inventory" or "stats" so I can't handle
    that part of equipment, but the way it works currently is you may equip
    a single headwear and a single piece of armor, as well as a number of
    accessories and two-one or one-two handed weapon(s).

--- Tactics/ui/unit_ui.py
+++ Tactics/ui/unit_ui.py
@@ -16,12 +16,32 @@ class UnitUI(ui.TacticsUI):
     def __init__(self, *args, **kwargs):
         super(UnitUI, self).__init__(*args, **kwargs)
         self.surface = pygame.Surface(SCREEN_SIZE)
+        self.leftsurf = pygame.Surface((470, 450))
+        self.rightsurf = EquipmentPage()
+        self.redraw_both()
+
         self.darksurface = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
         self.darksurface.fill((0, 0, 0, 255))
         self.darkstep = 0
+        
         self.mode = UnitUI.IN_MODE
         # do not redraw in here i guess; this screen does nothing
         # until set_unit is called.
+
+    def redraw_both(self):
+        self.redraw_left(False)
+        self.redraw_right(False)
+
+    def redraw_left(self, do_redraw=True):
+        self.leftsurf.fill((0, 0x66, 0xFF))
+        pygame.draw.rect(self.leftsurf, (0, ) * 3, ((0, 0), (470, 450)), 3)
+        if do_redraw:
+            self.redraw()
+
+    def redraw_right(self, do_redraw=True):
+        self.rightsurf.redraw()
+        if do_redraw:
+            self.redraw()
 
     def set_mode(self, mode):
         self.mode = mode
@@ -38,6 +58,8 @@ class UnitUI(ui.TacticsUI):
 
     def redraw(self):
         self.surface.fill((0, 0x33, 0xCC))
+        self.surface.blit(self.leftsurf, (26, 30))
+        self.surface.blit(self.rightsurf, (514, 30))
 
     def reblit(self, screen):
         screen.blit(self.surface, (0, 0))
@@ -71,3 +93,31 @@ class UnitUI(ui.TacticsUI):
     @staticmethod
     def name():
         return "unit"
+
+class UnitPages(pygame.Surface):
+    """ Nominally abstract """
+    def __init__(self):  # probably want to send the unit too
+        super(UnitPages, self).__init__((470, 450))
+        self.tabwidth = 33
+
+    def redraw(self, tabnum):
+        self.fill((0, 0x66, 0xFF))
+        pygame.draw.lines(self, (0, ) * 3, False,
+            ((tabnum * self.tabwidth, 0), (0, 0), (0, 450),
+             (470, 450), (470, 0), ((tabnum + 1) * self.tabwidth, 0)), 3)
+
+
+class EquipmentPage(UnitPages):
+    def __init__(self):
+        super(EquipmentPage, self).__init__()
+        self.font = pygame.font.Font(None, 40)
+        self.slots = ["Head", "Armor", "Mainhand", "Offhand", "Accessories"]
+        self.slot_width = max(map(lambda x: self.font.size(x)[0], self.slots))
+
+    def redraw(self):
+        super(EquipmentPage, self).redraw(0)
+        for i, slot in enumerate(self.slots):
+            slot_text = self.font.render(slot, True, (0xFF, ) * 3)
+            self.blit(slot_text, (self.slot_width - slot_text.get_size()[0] + 5,
+                i*(5+slot_text.get_size()[1]) + 5))
+
--- Tactics/unit.py
+++ Tactics/unit.py
@@ -20,6 +20,82 @@ class Stats(object):
         return float(self.cur_hp)/self.hp
 
 
+class EquipmentItem:
+    """ Currently just an example. """
+    def __init__(self):
+        self.equip_slot = "head"
+        self.item_type = "hat"
+        self.name = "Stylish Beret"
+
+
+class EquipmentSet:
+    def __init__(self):
+        self.equipment = {
+            "head":None,
+            "armor":None,
+            "mainhand":None,
+            "offhand":None,
+            "accessories":[]
+        }
+
+    def equip(self, item, slot=None):
+        """ slot is unnecessary except for mainhand vs offhand """
+        if item.equip_slot not in self.equipment:
+            return False
+        if item.equip_slot == "accessories":  # special case.
+            alike = 0
+            for i, it in enumerate(self.equipment["accessories"]):
+                if i != slot and it.item_type == item.item_type:
+                    alike += 1
+            if alike >= (2 if it.item_type == "ring" else 1):
+                return False  # ^ not the best
+            if slot is not None:    # overwriting old accessory
+                self._unequip_accessory(slot)
+                self.equipment["accessories"][slot] = item
+            else:   # adding new accessory
+                slot = len(self.equipment["accessories"])
+                self.equipment["accessories"].append(item)
+        else:   # not an accessory
+            if item.equip_slot in ("mainhand", "offhand"):
+                if item.hands == 1:  # slot is important
+                    if slot not in ("mainhand", "offhand"):
+                        return False
+                else:  # slot not important: takes both hands
+                    slot = "mainhand"
+                    self.unequip("offhand")
+                    self.equipment["offhand"] = True
+            else:
+                slot = item.equip_slot
+            self.unequip(slot)
+            self.equipment[slot] = item
+        # TODO: recalculate stats (based on item)
+
+    def _unequip_accessory(self, index):
+        """ Unequips the accessory at slot 'index', but does not shift
+        later elements back """
+        item = self.equipment["accessories"][index]
+        if item is not None:
+            # TODO: put back in your inventory
+            self.equipment["accessories"][index] = None
+            # TODO: recalculate stats (based on item)
+
+    def unequip(self, slot, acc_slot=None):
+        if slot in self.equipment:
+            if slot == "accessories":
+                self._unequip_accessory(acc_slot)
+                del self.equipment["accessory"][acc_slot]
+                return
+            if slot in ("mainhand", "offhand"):
+                if self.equipment["offhand"] is True:  # currently a two-hander
+                    self.equipment["offhand"] = None
+                    self.unequip("mainhand")
+            item = self.equipment[slot]
+            if item is not None:
+                # TODO: put it back in your inventory
+                self.equipment[slot] = None
+                # TODO: recalculate stats (based on item)
+
+
 class Unit(Stats):
     # TODO: late step: this loads all class's images
     # whether they're needed or not.
